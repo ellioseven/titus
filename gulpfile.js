0f3016985d8d03ae38b3a15858147c80ae949aca
Elliot Mitchum <elliot.mitchum@ellioseven.com>
2017-11-21 21:59:59 +1100

    Init sass workflow

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+var sourcemaps = require('gulp-sourcemaps');
+var sassGlob = require('gulp-sass-glob');
+var sasslint = require('gulp-sass-lint');
+
+gulp.task('sass', function () {
+  return gulp.src('./sass')
+    .pipe(sassGlob())
+    .pipe(sasslint({
+       configFile: '.sasslint.json'
+     }))
+    .pipe(sasslint.format())
+    .pipe(sasslint.failOnError())
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./css'))
+});
+
+gulp.task('sass:watch', function () {
+  watch('./sass', function () {
+    gulp.start('sass');
+  });
+});
