bf736af7a3148e46ddba17b3598a98f617fed8ee
Elliot Mitchum <elliot.mitchum@ellioseven.com>
2017-11-22 01:46:39 +1100

    Remove globbing

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,10 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const watch = require('gulp-watch');
 const sourcemaps = require('gulp-sourcemaps');
-const sassGlob = require('gulp-sass-glob');
 const sasslint = require('gulp-sass-lint');
 
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
-    .pipe(sassGlob())
     .pipe(sasslint({
        configFile: '.sasslintrc.json'
      }))
