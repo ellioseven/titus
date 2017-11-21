0d0e7992e3b8c85ba8f04ee3b26ea63f2a0efa05
Elliot Mitchum <elliot.mitchum@ellioseven.com>
2017-11-21 22:34:16 +1100

    Use new lint file

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,17 @@
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var watch = require('gulp-watch');
-var sourcemaps = require('gulp-sourcemaps');
-var sassGlob = require('gulp-sass-glob');
-var sasslint = require('gulp-sass-lint');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const watch = require('gulp-watch');
+const sourcemaps = require('gulp-sourcemaps');
+const sassGlob = require('gulp-sass-glob');
+const sasslint = require('gulp-sass-lint');
 
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
     .pipe(sassGlob())
     .pipe(sasslint({
-       configFile: '.sasslint.json'
+       configFile: '.sasslintrc.json'
      }))
     .pipe(sasslint.format())
     .pipe(sasslint.failOnError())
