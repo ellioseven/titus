e8f7362a366543f3e468f0eb1024815eb3ad651c
Elliot Mitchum <elliot.mitchum@ellioseven.com>
2017-12-27 22:48:17 +1100

    Remove margins on empty regions

--- js/dist/titus.js
+++ js/dist/titus.js
@@ -22,4 +22,36 @@
       });
     }
   };
+  /**
+   * Behaviors for applying empty region classes.
+   *
+   * @type {Drupal~behavior}
+   *
+   * @prop {Drupal~behaviorAttach} attach
+   *   Attaches behavior for hidden class additions.
+   */
+  Drupal.behaviors.titusEmptyRegionClasses = {
+    emptyClass: 'is-empty',
+    setClasses: function setClasses(element) {
+      var plugin = this;
+      var $element = $(element);
+      $element.removeClass(plugin.emptyClass);
+      if (!$element.children().length) {
+        $element.addClass(plugin.emptyClass);
+      }
+    },
+    attach: function attach(context) {
+      var plugin = this;
+      var $context = $(context);
+      $context.find('.region').each(function (index, element) {
+        plugin.setClasses(element);
+        var config = { childList: true };
+        var callback = function callback() {
+          return plugin.setClasses(element);
+        };
+        var observer = new MutationObserver(callback);
+        observer.observe(element, config);
+      });
+    }
+  };
 })(jQuery, Drupal, drupalSettings);
--- js/src/titus.es6.js
+++ js/src/titus.es6.js
@@ -20,4 +20,35 @@
       });
     },
   };
+  /**
+   * Behaviors for applying empty region classes.
+   *
+   * @type {Drupal~behavior}
+   *
+   * @prop {Drupal~behaviorAttach} attach
+   *   Attaches behavior for hidden class additions.
+   */
+  Drupal.behaviors.titusEmptyRegionClasses = {
+    emptyClass: 'is-empty',
+    setClasses(element) {
+      const plugin = this;
+      const $element = $(element);
+      $element.removeClass(plugin.emptyClass);
+      if (!$element.children().length) {
+        $element.addClass(plugin.emptyClass);
+      }
+    },
+    attach(context) {
+      const plugin = this;
+      const $context = $(context);
+      $context.find('.region').each((index, element) => {
+        plugin.setClasses(element);
+        // Observe child element additions/removals.
+        const config = { childList: true };
+        const callback = () => plugin.setClasses(element);
+        const observer = new MutationObserver(callback);
+        observer.observe(element, config);
+      });
+    },
+  };
 }(jQuery, Drupal, drupalSettings));
